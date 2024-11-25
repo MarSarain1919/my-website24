e81a984be67637ad71ffd0fecb87360934601c78
Katja Sarain <katjasarain@gmail.com>
2024-11-25 15:00:23 -0800

    Added dropdowns

--- assets/app.js
+++ assets/app.js
@@ -3,3 +3,17 @@ document.getElementById('changeTextButton').addEventListener('click', function()
     const content = document.getElementById('content');
     content.innerHTML = '<p>You clicked the button! The text has changed.</p>';
 });
+
+<script>
+    document.querySelectorAll("details").forEach((detail) => {
+        detail.addEventListener("toggle", () => {
+            if (detail.open) {
+                document.querySelectorAll("details").forEach((otherDetail) => {
+                    if (otherDetail !== detail) {
+                        otherDetail.open = false;
+                    }
+                });
+            }
+        });
+    });
+</script>
