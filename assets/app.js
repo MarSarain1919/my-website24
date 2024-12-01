644c90c2291e0ba94cbfa22fbd0dcafe0126bd06
Katja Sarain <katjasarain@gmail.com>
2024-11-30 19:06:55 -0800

    corrected path

--- assets/app.js
+++ assets/app.js
@@ -2,11 +2,11 @@
 const imageContainer = document.getElementById('image-container');
 
 const image1 = document.createElement('img');
-image1.src = 'baked.jpeg';
+image1.src = 'images/baked.jpeg';
 image1.alt = 'Image 1';
 
 const image2 = document.createElement('img');
-image2.src = 'lion.png';
+image2.src = 'images/lion.png';
 image2.alt = 'Image 2';
 
 // Append the images to the container
