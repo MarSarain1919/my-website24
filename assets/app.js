ef4c3f984ad0bde3cf8f8124455d2780f8fb7670
Katja Sarain <katjasarain@gmail.com>
2024-11-30 19:04:18 -0800

    added images using JavaScript

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+// Create two image elements using JavaScript
+const imageContainer = document.getElementById('image-container');
+
+const image1 = document.createElement('img');
+image1.src = 'baked.jpeg';
+image1.alt = 'Image 1';
+
+const image2 = document.createElement('img');
+image2.src = 'lion.png';
+image2.alt = 'Image 2';
+
+// Append the images to the container
+imageContainer.appendChild(image1);
+imageContainer.appendChild(image2);
