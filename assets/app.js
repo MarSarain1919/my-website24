d18a74b4b17f29d4d59d2e30f9bf7dd5ff0b4507
Katja Sarain <katjasarain@gmail.com>
2024-11-30 19:08:16 -0800

    fixing errors

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,13 @@
 // Create two image elements using JavaScript
-const imageContainer = document.getElementById('image-container');
+const imageContainer=document.getElementById('image-container');
 
-const image1 = document.createElement('img');
-image1.src = 'images/baked.jpeg';
-image1.alt = 'Image 1';
+const image1=document.createElement('img');
+image1.src='images/baked.jpeg';
+image1.alt='Image 1';
 
-const image2 = document.createElement('img');
-image2.src = 'images/lion.png';
-image2.alt = 'Image 2';
+const image2=document.createElement('img');
+image2.src='images/lion.png';
+image2.alt='Image 2';
 
 // Append the images to the container
 imageContainer.appendChild(image1);
